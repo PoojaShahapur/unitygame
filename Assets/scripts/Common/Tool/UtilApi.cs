d727015de9fd886d6ba82826a12a650638353d13
aaa <aaa@163.com>
2014-11-03 19:44:52 +0800

    修改

---
+++ Assets/scripts/Common/Tool/UtilApi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace SDK.Common
+{
+    /**
+     * @brief 对 api 的进一步 wrap 
+     */
+    public class UtilApi
+    {
+        // 通过父对象和完整的目录查找 child 对象
+        static public GameObject TransFindChildByPObjAndPath(GameObject pObject, string path)
+        {
+            return pObject.transform.Find(path).gameObject;
+        }
+
+        // 仅仅根据名字查找 GameObject ，注意如果 GameObject 设置 SetActive 为 false ，就会查找不到，如果有相同名字的 GameObject ，不保证查找到正确的。
+        static public GameObject GoFindChildByPObjAndName(string name)
+        {
+            return GameObject.Find(name);
+        }
+    }
+}
