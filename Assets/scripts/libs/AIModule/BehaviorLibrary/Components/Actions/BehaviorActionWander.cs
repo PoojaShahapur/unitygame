9e16d0a97fb45113d8f339618a1c1db9284566a2
kuangben <kuangben2010@yeah.net>
2014-11-08 00:12:06 +0800

    修改

---
+++ Assets/scripts/libs/AIModule/BehaviorLibrary/Components/Actions/BehaviorActionWander.cs
@@ -0,0 +1,20 @@
+﻿namespace BehaviorLibrary.Components.Actions
+{
+    /**
+     * @brief 徘徊
+     */
+    public class BehaviorActionPatrol : BehaviorAction
+    {
+        public BehaviorActionPatrol()
+            : base(null)
+        {
+            
+        }
+
+        public override BehaviorReturnCode Behave(InsParam inputParam)
+        {
+
+            return BehaviorReturnCode.Success;
+        }
+    }
+}
---
+++ Assets/scripts/libs/AIModule/BehaviorLibrary/InsParam.cs
@@ -0,0 +1,10 @@
+﻿namespace BehaviorLibrary
+{
+    /**
+     * @brief 实例参数，行为树执行时传入的操作数据
+     */
+    public class InsParam
+    {
+
+    }
+}
