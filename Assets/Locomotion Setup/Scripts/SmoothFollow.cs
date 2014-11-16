07d812556e8a92b29f4e6b1539703b90c02742b6
kuangben <kuangben2010@yeah.net>
2014-11-16 11:09:00 +0800

    修改

--- Assets/Locomotion Setup/Scripts/SmoothFollow.cs
+++ Assets/Locomotion Setup/Scripts/SmoothFollow.cs
@@ -8,51 +8,52 @@ For every of those smoothed values we calculate the wanted value and the current
 Then we smooth it using the Lerp function.
 Then we apply the smoothed values to the transform's position.
 */
+using UnityEngine;
 
-// The target we are following
-var target : Transform;
-// The distance in the x-z plane to the target
-var distance = 2.75;
-// the height we want the camera to be above the target
-var height = 2.5;
-// How much we 
-var heightDamping = 2.0;
-var rotationDamping = 0.0;
-var lookAtAngle = 0.0;
-
-// Place the script in the Camera-Control group in the component menu
-@script AddComponentMenu("Camera-Control/Smooth Follow")
-
-
-function LateUpdate () {
-	// Early out if we don't have a target
-	if (!target)
-		return;
+public class SmoothFollow : MonoBehaviour
+{
+	// The target we are following
+	public Transform target;
+	// The distance in the x-z plane to the target
+	public float distance = 2.75f;
+	// the height we want the camera to be above the target
+	public float height = 2.5f;
+	// How much we 
+	public float heightDamping = 2.0f;
+	public float rotationDamping = 0.0f;
+	public float lookAtAngle = 0.0f;
 	
-	// Calculate the current rotation angles
-	var wantedRotationAngle : float = target.eulerAngles.y;
-	var wantedHeight : float = target.position.y + height;
+	void LateUpdate () {
+		// Early out if we don't have a target
+		if (!target)
+			return;
 		
-	var currentRotationAngle : float = lookAtAngle; //transform.eulerAngles.y + 
-	var currentHeight : float = transform.position.y;
-	
-	// Damp the rotation around the y-axis
-	currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+		// Calculate the current rotation angles
+		float wantedRotationAngle = target.eulerAngles.y;
+		float wantedHeight = target.position.y + height;
+			
+		float currentRotationAngle = lookAtAngle; //transform.eulerAngles.y + 
+		float currentHeight  = transform.position.y;
+		
+		// Damp the rotation around the y-axis
+		currentRotationAngle = Mathf.LerpAngle (currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
 
-	// Damp the height
-	currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+		// Damp the height
+		currentHeight = Mathf.Lerp (currentHeight, wantedHeight, heightDamping * Time.deltaTime);
 
-	// Convert the angle into a rotation
-	var currentRotation : Quaternion = Quaternion.Euler (0, currentRotationAngle, 0);
-	
-	// Set the position of the camera on the x-z plane to:
-	// distance meters behind the target
-	transform.position = target.position;
-	transform.position -= currentRotation * Vector3.forward * distance;
+		// Convert the angle into a rotation
+		Quaternion currentRotation = Quaternion.Euler (0, currentRotationAngle, 0);
+		
+		// Set the position of the camera on the x-z plane to:
+		// distance meters behind the target
+		transform.position = target.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
 
-	// Set the height of the camera
-	transform.position.y = currentHeight;
-	
-	// Always look at the target
-	transform.LookAt (target);
+		// Set the height of the camera
+		//transform.position.y = currentHeight;
+		transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
+
+		// Always look at the target
+		transform.LookAt (target);
+	}
 }
