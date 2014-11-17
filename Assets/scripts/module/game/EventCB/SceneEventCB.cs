885024094e17ecb772a05b513aa26d90e180188b
aaa <aaa@163.com>
2014-11-17 15:49:35 +0800

    修改

--- Assets/scripts/module/game/EventCB/SceneEventCB.cs
+++ Assets/scripts/module/game/EventCB/SceneEventCB.cs
@@ -14,7 +14,8 @@ namespace Game.Game
             IPlayerMain playerMain = Ctx.m_instance.m_playerMgr.createHero();
             Ctx.m_instance.m_playerMgr.addHero(playerMain);
             //playerMain.setSkeleton("DefaultAvatar_Unity_Body_Mesh");
-            playerMain.setSkeleton("DefaultAvatar");
+            //playerMain.setSkeleton("DefaultAvatar");
+            playerMain.setSkeleton("TestBeing");
         }
     }
 }
