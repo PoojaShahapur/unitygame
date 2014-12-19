97043c9250582d350835eaac21407df2beaf27f5
kuangben <kuangben2010@yeah.net>
2014-12-19 22:51:55 +0800

    提交

--- Assets/scripts/libs/FrameWork/ProcessSys.cs
+++ Assets/scripts/libs/FrameWork/ProcessSys.cs
@@ -8,9 +8,9 @@ namespace SDK.Lib
 {
     class ProcessSys : IProcessSys
     {
-        protected float m_LastSec = 0f;         // 上一次更新时的秒数
-        protected float m_curSec = 0f;          // 正在获取的时间
-        protected float mDeltaSec = 0f;         // 两帧之间的间隔
+        protected long m_preTime = 0;         // 上一次更新时的秒数
+        protected long m_curTime = 0;          // 正在获取的时间
+        protected float m_deltaSec = 0f;         // 两帧之间的间隔
 
         public ProcessSys()
         {
@@ -19,13 +19,14 @@ namespace SDK.Lib
 
         public void ProcessNextFrame()
         {
-            m_curSec = DateTime.Now.Second;
-            if(m_LastSec != 0f)     // 第一帧跳过，因为这一帧不好计算间隔
+            m_curTime = DateTime.Now.Ticks;
+            if (m_preTime != 0f)     // 第一帧跳过，因为这一帧不好计算间隔
             {
-                mDeltaSec = m_curSec - m_LastSec;
-                Advance(mDeltaSec);
+                TimeSpan ts = new TimeSpan(m_curTime - m_preTime);
+                m_deltaSec = (float)(ts.TotalSeconds);
+                Advance(m_deltaSec);
             }
-            m_LastSec = m_curSec;
+            m_preTime = m_curTime;
         }
 
         public void Advance(float delta)
