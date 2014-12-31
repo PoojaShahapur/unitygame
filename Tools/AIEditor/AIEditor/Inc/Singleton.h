4d5bd8060d1d68cd1f0ab47e1cf3aa96eb1578f1
kuangben <kuangben2010@yeah.net>
2015-01-01 00:20:35 +0800

    提交

---
+++ Tools/AIEditor/AIEditor/Inc/Singleton.h
@@ -0,0 +1,27 @@
+﻿#ifndef __SINGLETON_H
+#define __SINGLETON_H
+
+#include "AIEditor.h"
+
+BEGIN_NAMESPACE_AIEDITOR
+
+template<class T>
+class AIEDITOR_EXPORT Singleton
+{
+public:
+	static T* m_sSingleton;
+
+public:
+	static T* getSingletonPtr()
+	{
+		if (m_sSingleton == NULL)
+		{
+			m_sSingleton = new T();
+		}
+		return m_sSingleton;
+	}
+};
+
+END_NAMESPACE_AIEDITOR
+
+#endif				// SINGLETON_H
