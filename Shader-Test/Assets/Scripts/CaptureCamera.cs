a36928155063da26c210e522795e0ad660985cc7
aaa <kuangben2001@163.com>
2015-03-19 19:04:36 +0800

    提交

---
+++ Shader-Test/Assets/Scripts/CaptureCamera.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureCamera : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        //设置按钮中文字的颜色  
+        GUI.color = Color.green;
+        //设置按钮的背景色  
+        GUI.backgroundColor = Color.red;
+
+        if (GUI.Button(new Rect(100, 100, 300, 40), "捕捉摄像机区域图像"))
+        {
+            Capture(Camera.main, new Rect(0, 0, Screen.width, Screen.height));
+        }
+    }
+
+    Texture2D Capture(Camera camera, Rect rect)
+    {
+        // 创建一个RenderTexture对象
+        RenderTexture rt = new RenderTexture((int)rect.width, (int)rect.height, 0);
+        // 临时设置相关相机的targetTexture为rt, 并手动渲染相关相机
+        camera.targetTexture = rt;
+        camera.Render();
+        //ps: --- 如果这样加上第二个相机，可以实现只截图某几个指定的相机一起看到的图像。
+        //ps: camera2.targetTexture = rt;
+        //ps: camera2.Render();
+        //ps: -------------------------------------------------------------------
+
+        // 激活这个rt, 并从中中读取像素。
+        RenderTexture.active = rt;
+        Texture2D screenShot = new Texture2D((int)rect.width, (int)rect.height, TextureFormat.ARGB32, false);
+        screenShot.ReadPixels(rect, 0, 0);// 注：这个时候，它是从RenderTexture.active中读取像素
+        screenShot.Apply();
+
+        // 重置相关参数，以使用camera继续在屏幕上显示
+        camera.targetTexture = null;
+        //ps: camera2.targetTexture = null;
+        RenderTexture.active = null; // JC: added to avoid errors
+        GameObject.Destroy(rt);
+        // 最后将这些纹理数据，成一个png图片文件  
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = Application.dataPath + "/Screenshot.png";
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log(string.Format("截屏了一张照片: {0}", filename));
+
+        return screenShot;
+    }
+}
---
+++ Shader-Test/Assets/Shaders/AlphaTestDiffuse.shader
@@ -0,0 +1,33 @@
+﻿Shader "Custom/AlphaTestDiffuse" {
+    Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		//_Cutoff ("Alpha cutoff", Range (0,1)) = 2
+	}
+    SubShader {
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+		//AlphaTest Equal [_Cutoff]
+		Lighting Off
+		Blend OneMinusDstAlpha DstAlpha
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Lambert
+
+        sampler2D _MainTex;
+
+        struct Input {
+            float2 uv_MainTex;
+        };
+
+        void surf (Input IN, inout SurfaceOutput o) {
+            half4 c = tex2D (_MainTex, IN.uv_MainTex);
+            o.Albedo = c.rgb;
+            //o.Alpha = c.a;
+			o.Alpha = 1.0f;
+			//clip (o.Alpha - _Cutoff);
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
