3562d3ee3b9f9db14cc33c5b3ba92a44833b45c7
kuangben <kuangben2010@yeah.net>
2015-03-29 13:44:40 +0800

    aaa

--- Client/Assets/Scripts/Module/Login/LoginSys.cs
+++ Client/Assets/Scripts/Module/Login/LoginSys.cs
@@ -10,7 +10,7 @@ namespace Game.Login
     public class LoginSys
     {
         static public LoginSys m_instance;
-        public Ctx m_ctx;
+        //public Ctx m_ctx;
         public LoginFlowHandle m_loginFlowHandle;       // 整个登陆流程处理
 
         public void Start()
@@ -22,9 +22,9 @@ namespace Game.Login
         public void initGVar()
         {
             // 获取全局变量
-            GameObject nodestroy = UtilApi.GoFindChildByPObjAndName(NotDestroyPath.ND_CV_App);
-            AppRoot approot = nodestroy.GetComponent<AppRoot>();
-            LoginSys.m_instance.m_ctx = approot.getCtx();
+            //GameObject nodestroy = UtilApi.GoFindChildByPObjAndName(NotDestroyPath.ND_CV_App);
+            //AppRoot approot = nodestroy.GetComponent<AppRoot>();
+            //LoginSys.m_instance.m_ctx = approot.getCtx();
 
             // 游戏逻辑处理
             Ctx.m_instance.m_cbUIEvent = new LoginUIEventCB();
