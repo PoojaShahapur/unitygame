07245afd895b9d295f83b634fc2a5eafbb32b0c1
kuangben <kuangben2010@yeah.net>
2015-04-04 22:31:34 +0800

    aaa

---
+++ Client/Assets/Scripts/UnitTestSrc/VersionTest/TestVersion.cs
@@ -0,0 +1,17 @@
+﻿using SDK.Common;
+namespace UnitTestSrc
+{
+    public class TestVersion
+    {
+        public void run()
+        {
+            testVersion();
+        }
+
+        protected void testVersion()
+        {
+            //Ctx.m_instance.m_versionSys.loadVerFile();
+            Ctx.m_instance.m_versionSys.loadMiniVerFile();
+        }
+    }
+}
