86d29ddc5e13463dae1b52588608e60242c11dc7
kuangben <kuangben2010@yeah.net>
2015-04-05 23:10:28 +0800

    aaa

--- Client/Assets/Scripts/Libs/Resource/AutoUpdate/AutoUpdateSys.cs
+++ Client/Assets/Scripts/Libs/Resource/AutoUpdate/AutoUpdateSys.cs
@@ -1,4 +1,5 @@
 ﻿using SDK.Common;
+using System.Collections.Generic;
 namespace SDK.Lib
 {
     /**
@@ -6,6 +7,10 @@ namespace SDK.Lib
      */
     public class AutoUpdateSys
     {
+        public List<string> m_loadingPath = new List<string>();
+        public List<string> m_loadedPath = new List<string>();
+        public List<string> m_failedPath = new List<string>();
+
         public void loadMiniVersion()
         {
             Ctx.m_instance.m_versionSys.m_miniLoadResultDisp = miniVerLoadResult;
@@ -23,13 +28,67 @@ namespace SDK.Lib
             if(Ctx.m_instance.m_versionSys.m_needUpdateVer) // 如果需要更新
             {
                 // 开始正式加载文件
-                loadUpdateFile();
+                loadAllUpdateFile();
             }
         }
 
-        public void loadUpdateFile()
+        public void loadAllUpdateFile()
         {
+            foreach (KeyValuePair<string, FileVerInfo> kv in Ctx.m_instance.m_versionSys.m_webVer.m_path2HashDic)
+            {
+                if(Ctx.m_instance.m_versionSys.m_localVer.m_path2HashDic.ContainsKey(kv.Key))
+                {
+                    if(Ctx.m_instance.m_versionSys.m_localVer.m_path2HashDic[kv.Key].m_fileMd5 != kv.Value.m_fileMd5)
+                    {
+                        loadOneUpdateFile(kv.Key, kv.Value);
+                    }
+                }
+            }
+        }
 
+        public void loadOneUpdateFile(string path, FileVerInfo fileInfo)
+        {
+            m_loadingPath.Add(path);
+            UtilApi.delFileNoVer(path);     // 删除当前目录下已经有的 old 文件
+
+            LoadParam param = Ctx.m_instance.m_poolSys.newObject<LoadParam>();
+            param.m_path = path;
+
+            param.m_resLoadType = ResLoadType.eLoadWeb;
+            param.m_version = fileInfo.m_fileMd5;
+
+            param.m_loaded = onLoaded;
+            param.m_failed = onFailed;
+
+            Ctx.m_instance.m_resLoadMgr.loadData(param);
+            Ctx.m_instance.m_poolSys.deleteObj(param);
+        }
+
+        protected void onLoaded(IDispatchObject resEvt)
+        {
+            m_loadedPath.Add((resEvt as DataResItem).path);
+            m_loadingPath.Remove((resEvt as DataResItem).path);
+
+            if(m_loadingPath.Count == 0)
+            {
+                onUpdateEnd();
+            }
+        }
+
+        protected void onFailed(IDispatchObject resEvt)
+        {
+            m_failedPath.Add((resEvt as DataResItem).path);
+            m_loadingPath.Remove((resEvt as DataResItem).path);
+
+            if (m_loadingPath.Count == 0)
+            {
+                onUpdateEnd();
+            }
+        }
+
+        protected void onUpdateEnd()
+        {
+            // 进入游戏
         }
     }
 }
