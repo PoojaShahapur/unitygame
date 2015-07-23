41a28d878903075a34ee93d6c9d727ae42858698
aaa <kuangben2001@163.com>
2015-07-23 22:11:21 +0800

    添加 lua 工具

--- Client/Assets/Prefabs/Resources/LuaScript/DataStruct/ByteBuffer.lua
+++ Client/Assets/Prefabs/Resources/LuaScript/DataStruct/ByteBuffer.lua
@@ -1,167 +1,212 @@
 --[[字节缓冲区]]
-ByteBuffer = {}
+require('DataStruct/Class')
+ByteBuffer = class()    -- 定义一个类，必须从返回的类中添加成员
 
 ByteBuffer.ENDIAN_LITTLE = 0    -- 小端字节序是 0
 ByteBuffer.ENDIAN_BIG = 1       -- 大端字节序是 0
-ByteBuffer.m_endian = ByteBuffer.ENDIAN_LITTLE -- 自己字节序
+-- ByteBuffer.m_endian = ByteBuffer.ENDIAN_LITTLE -- 自己字节序
 ByteBuffer.m_sysEndian = ByteBuffer.ENDIAN_LITTLE -- 系统字节序
 
-ByteBuffer.m_buff = {}  -- 字节缓冲区
-ByteBuffer.m_position = 1   -- 缓冲区当前位置，注意 Lua 下表是从 1 开始的，不是从 0 开始的。 this.m_buff[0] == nil ，太坑了
+-- ByteBuffer.m_buff = {}  -- 字节缓冲区
+-- ByteBuffer.m_position = 1   -- 缓冲区当前位置，注意 Lua 下表是从 1 开始的，不是从 0 开始的。 self.m_buff[0] == nil ，太坑了
 
-local this = ByteBuffer   -- 局部引用
+-- local self = ByteBuffer   -- 局部引用
+
+function ByteBuffer:ctor()  -- 定义 ByteBuffer 的构造函数
+    self.m_endian = ByteBuffer.ENDIAN_LITTLE -- 自己字节序
+    self.m_buff = {}  -- 字节缓冲区
+    self.m_position = 1   -- 缓冲区当前位置，注意 Lua 下表是从 1 开始的，不是从 0 开始的。 self.m_buff[0] == nil ，太坑了
+end
+
+function ByteBuffer:setEndian(endian)
+    self.m_endian = endian
+end
 
 -- 读取一个字节
-function ByteBuffer.readInt8()
-    local elem = this.m_buff[this.m_position]
+function ByteBuffer:readInt8()
+    local elem = self.m_buff[self.m_position]
     local retData = string.byte(elem)
-    this.advPos(1);
+    self.advPos(1);
     return retData
 end
 
 -- 读取和写入的时候只看存储时候的字节序就行了，不用管系统字节序，因为是自组合成本地数据的
 -- 读取两个字节
-function ByteBuffer.readInt16()
+function ByteBuffer:readInt16()
     local retData = 0
-    if this.canRead(2) then
-        if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-            retData = string.byte(this.m_buff[this.m_position]) * 256 + string.byte(this.m_buff[this.m_position + 1])
+    if self.canRead(2) then
+        if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+            retData = string.byte(self.m_buff[self.m_position]) * 256 + string.byte(self.m_buff[self.m_position + 1])
         else
-            retData = string.byte(this.m_buff[this.m_position + 1]) * 256 + string.byte(this.m_buff[this.m_position])
+            retData = string.byte(self.m_buff[self.m_position + 1]) * 256 + string.byte(self.m_buff[self.m_position])
         end
-        this.advPos(2);
+        self.advPos(2);
     end
     
     return retData
 end
 
-function ByteBuffer.readInt32()
+function ByteBuffer:readInt32()
     local retData = 0
-    if this.canRead(4) then
-        if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-            retData = string.byte(this.m_buff[this.m_position]) * 256 * 256 * 256 + string.byte(this.m_buff[this.m_position + 1]) * 256 * 256 + string.byte(this.m_buff[this.m_position + 2]) * 256 + string.byte(this.m_buff[this.m_position + 3])
+    if self.canRead(4) then
+        if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+            retData = string.byte(self.m_buff[self.m_position]) * 256 * 256 * 256 + string.byte(self.m_buff[self.m_position + 1]) * 256 * 256 + string.byte(self.m_buff[self.m_position + 2]) * 256 + string.byte(self.m_buff[self.m_position + 3])
         else
-            retData = string.byte(this.m_buff[this.m_position + 3]) * 256 * 256 * 256 + string.byte(this.m_buff[this.m_position + 2]) * 256 * 256 + string.byte(this.m_buff[this.m_position + 1]) * 256 + string.byte(this.m_buff[this.m_position])
+            retData = string.byte(self.m_buff[self.m_position + 3]) * 256 * 256 * 256 + string.byte(self.m_buff[self.m_position + 2]) * 256 * 256 + string.byte(self.m_buff[self.m_position + 1]) * 256 + string.byte(self.m_buff[self.m_position])
         end
-        this.advPos(4);
+        self.advPos(4);
     end
     
     return retData
 end
 
-function ByteBuffer.readNumber()
+function ByteBuffer:readNumber()
     local retData = 0
-    if this.canRead(8) then
-        if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-            local str = this.m_position[this.m_position] .. this.m_position[this.m_position + 1] .. this.m_position[this.m_position + 2] .. this.m_position[this.m_position + 3] .. this.m_position[this.m_position + 4] .. this.m_position[this.m_position + 5] .. this.m_position[this.m_position + 6] .. this.m_position[this.m_position + 7]
+    if self.canRead(8) then
+        if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+            local str = self.m_buff[self.m_position] .. self.m_buff[self.m_position + 1] .. self.m_buff[self.m_position + 2] .. self.m_buff[self.m_position + 3] .. self.m_buff[self.m_position + 4] .. self.m_buff[self.m_position + 5] .. self.m_buff[self.m_position + 6] .. self.m_buff[self.m_position + 7]
         else
-            local str = this.m_position[this.m_position + 7] .. this.m_position[this.m_position + 6] .. this.m_position[this.m_position + 5] .. this.m_position[this.m_position + 4] .. this.m_position[this.m_position + 3] .. this.m_position[this.m_position + 2] .. this.m_position[this.m_position + 1] .. this.m_position[this.m_position]
+            local str = self.m_buff[self.m_position + 7] .. self.m_buff[self.m_position + 6] .. self.m_buff[self.m_position + 5] .. self.m_buff[self.m_position + 4] .. self.m_buff[self.m_position + 3] .. self.m_buff[self.m_position + 2] .. self.m_buff[self.m_position + 1] .. self.m_buff[self.m_position]
         end
 
         retData = tonumber(str)        
-        this.advPos(8);
+        self.advPos(8);
     end
     
     return retData
 end
 
-function ByteBuffer.writeInt8(retData)
-    this.m_buff[this.m_position] = retData
-    this.advPosAndLen(1);
+-- 读取 utf-8 字符串
+function ByteBuffer:readMultiByte(len)
+    if self.canRead(len) then
+        local utf8Str
+        idx = 1
+        while(idx <= len)
+        do
+            if utf8Str == nil then
+                utf8Str = string.char(self.m_buff[self.m_position + idx - 1])
+            else
+                utf8Str = utf8Str .. string.char(self.m_buff[self.m_position + idx - 1])
+            end 
+        end
+        
+        self.advPos(len);
+    end
+    
+    return utf8Str
+end
+
+function ByteBuffer:writeInt8(retData)
+    self.m_buff[self.m_position] = retData
+    self.advPosAndLen(1);
 end
 
 function ByteBuffer.writeInt16(retData)
     local oneByte = retData % 256
     local twoByte = retData / 256
 
-    if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-        this.m_buff[this.m_position] = twoByte
-        this.m_buff[this.m_position + 1] = oneByte
+    if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+        self.m_buff[self.m_position] = twoByte
+        self.m_buff[self.m_position + 1] = oneByte
     else
-        this.m_buff[this.m_position] = oneByte
-        this.m_buff[this.m_position + 1] = twoByte
+        self.m_buff[self.m_position] = oneByte
+        self.m_buff[self.m_position + 1] = twoByte
     end
     
-    this.advPosAndLen(2);
+    self.advPosAndLen(2);
 end
 
-function ByteBuffer.writeInt32(retData)
+function ByteBuffer:writeInt32(retData)
     local oneByte = retData % 256
     local twoByte = retData / 256 % 256
     local threeByte = retData / (256 * 256) % 256
     local fourByte = retData / (256 * 256 * 256)
 
-    if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-        this.m_buff[this.m_position] = fourByte
-        this.m_buff[this.m_position + 1] = threeByte
-        this.m_buff[this.m_position + 2] = twoByte
-        this.m_buff[this.m_position + 3] = oneByte
+    if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+        self.m_buff[self.m_position] = fourByte
+        self.m_buff[self.m_position + 1] = threeByte
+        self.m_buff[self.m_position + 2] = twoByte
+        self.m_buff[self.m_position + 3] = oneByte
     else
-        this.m_buff[this.m_position] = oneByte
-        this.m_buff[this.m_position + 1] = twoByte
-        this.m_buff[this.m_position + 2] = threeByte
-        this.m_buff[this.m_position + 3] = fourByte
+        self.m_buff[self.m_position] = oneByte
+        self.m_buff[self.m_position + 1] = twoByte
+        self.m_buff[self.m_position + 2] = threeByte
+        self.m_buff[self.m_position + 3] = fourByte
     end
     
-    this.advPosAndLen(4);
+    self.advPosAndLen(4);
 end
 
-function ByteBuffer.writeNumber(retData)
+function ByteBuffer:writeNumber(retData)
     str = tostrng(retData)
     len = string.len(str)
-    idx = 0
-    if ByteBuffer.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
-        while( idx < 8 )
+    idx = 1
+    if self.m_endian == ByteBuffer.ENDIAN_LITTLE then-- 如果是小端字节序
+        while( idx <= 8 )
         do
-            this.m_buff[this.m_position + idx] = string.byte(str, idx)
+            self.m_buff[self.m_position + idx - 1] = string.byte(str, idx)
             idx = idx + 1
         end
         
-        while( idx < 8 )
+        while( idx <= 8 )
         do
-            this.m_buff[this.m_position + idx] = 0
+            self.m_buff[self.m_position + idx - 1] = 0
             idx = idx + 1
         end
     else
         while( idx < 8 )
         do
-            this.m_buff[this.m_position + idx] = string.byte(str, len - idx)
+            self.m_buff[self.m_position + idx - 1] = string.byte(str, len - idx)
             idx = idx + 1
         end
         
         while( idx < 8 )
         do
-            this.m_buff[this.m_position + idx] = 0
+            self.m_buff[self.m_position + idx - 1] = 0
             idx = idx + 1
         end
     end
     
-    this.advPosAndLen(8);
+    self.advPosAndLen(8);
+end
+
+-- 写 utf-8 字节字符串，必须是 utf-8 的
+function ByteBuffer:writeMultiByte(value)
+    if value ~= nil then
+        idx = 1
+        while(idx <= string.len(value))
+        do
+            self.m_buff[self.m_position + idx - 1] = string.byte(string.sub(value, idx, 1))
+            idx = idx + 1
+        end
+    end
+    
+    self.advPosAndLen(string.len(value));
 end
 
 -- 是否有足够的字节可以读取
-function ByteBuffer.canRead(len)
-    if this.m_position + len > this.length() then
+function ByteBuffer:canRead(len)
+    if self.m_position + len > self.length() then
         return false
     end
     
     return true
 end
 
-function ByteBuffer.advPos(num)
-    this.m_position = this.m_position + num;
+function ByteBuffer:advPos(num)
+    self.m_position = self.m_position + num;
 end
 
-function ByteBuffer.advPosAndLen(num)
-    this.m_position = this.m_position + num;
+function ByteBuffer:advPosAndLen(num)
+    self.m_position = self.m_position + num;
 end
 
 -- 判断字节序和系统字节序是否相同
-function ByteBuffer.isEqualEndian()
-    return ByteBuffer.m_endian == ByteBuffer.m_sysEndian
+function ByteBuffer:isEqualEndian()
+    return self.m_endian == ByteBuffer.m_sysEndian
 end
 
 -- 获取长度
-function ByteBuffer.length()
-    return #this.m_buff
+function ByteBuffer:length()
+    return #self.m_buff
 end
--- Client/Assets/Prefabs/Resources/LuaScript/Test/TestByteBuffer.lua
+++ Client/Assets/Prefabs/Resources/LuaScript/Test/TestByteBuffer.lua
@@ -1,13 +1,23 @@
 -- 测试字节缓冲区
 
 local packagePath = package.path
-package.path = string.format("%s;%s/?.lua", packagePath, "E:/Self/Self/LuaByteBuffer-git/lua")
-require('LuaLib/ByteBuffer')
+package.path = string.format("%s;%s/?.lua", packagePath, "E:/Self/Self/unity/unitygame/Client/Assets/Prefabs/Resources/LuaScript")
 
-ByteBuffer.m_buff = {"22", 1, '2', 3, 4, 5, 6, 7, 8, 9}
-local retInt8 = 0
-local _int8 = ByteBuffer.readInt8(retInt8)
-local _int16 = ByteBuffer.readInt16()
+require('DataStruct/ByteBuffer')
+require('DataStruct/Class')
 
-print(_int8)
-print(_int16)
+ByteBuffer.m_buff = {3, 1, '2', 3, 4, 5, 6, 7, 8, 9}
+
+-- local _int8 = ByteBuffer.readInt8()
+-- local _int16 = ByteBuffer.readInt16()
+
+-- print(_int8)
+-- print(_int16)
+
+--local buIns = class(ByteBuffer)
+--a = buIns.new(1)
+a = ByteBuffer.new()
+a:writeMultiByte("aaaa")
+
+b = ByteBuffer.new()
+b:writeMultiByte("bbb")
