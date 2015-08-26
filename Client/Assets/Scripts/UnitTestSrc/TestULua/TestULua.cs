1a02cb9b14917b105585031e94faaf084ea33421
aaa <kuangben2001@163.com>
2015-08-26 14:42:46 +0800

    aaa

---
+++ Client/Assets/Scripts/UnitTestSrc/TestULua/TestULua.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestULua : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LuaScriptMgr lua = new LuaScriptMgr();
+        lua.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
