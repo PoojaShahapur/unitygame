106fb439570af54e7d067c13016d32733b3b4132
aaa <kuangben2001@163.com>
2015-12-04 15:28:23 +0800

    修改地形

--- Client/Assets/Scripts/Libs/Mesh/Cameras/Lenses/MPerspectiveLens.cs
+++ Client/Assets/Scripts/Libs/Mesh/Cameras/Lenses/MPerspectiveLens.cs
@@ -48,15 +48,16 @@
          */
         override public void updateMatrix()
 		{
-            float halfHeight = m_nearDist * m_focalLengthInv;
-            float halfWidth = halfHeight * m_aspectRatio;
+            // 计算投影矩阵，投影矩阵使用的是 [-1, 1] x [-1, 1] x [-1, 1]，使用 OpenGL 的投影矩阵
+            float nearHalfHeight = m_nearDist * m_focalLengthInv;
+            float nearHalfWidth = nearHalfHeight * m_aspectRatio;
 
             float left = 0, right = 0, top = 0, bottom = 0;
 			
-			left = -halfWidth;
-			right = halfWidth;
-			top = halfHeight;
-			bottom = -halfHeight;
+			left = -nearHalfWidth;
+			right = nearHalfWidth;
+			top = nearHalfHeight;
+			bottom = -nearHalfHeight;
 
             float invWidth = 1 / (right - left);
             float invHeight = 1 / (top - bottom);
@@ -72,18 +73,30 @@
             m_matrix3D.m[0, 1] = m_matrix3D.m[0, 3] = m_matrix3D.m[1, 0] = m_matrix3D.m[1, 3] = m_matrix3D.m[2, 0] = m_matrix3D.m[2, 1] =
                 m_matrix3D.m[3, 0] = m_matrix3D.m[3, 1] = m_matrix3D.m[3, 3] = 0;
 
-			float yMaxFar = m_farDist * m_focalLengthInv;
-            float xMaxFar = yMaxFar* m_aspectRatio;
+            // 更新 Frustum 的八个顶点，这个是相机空间的坐标位置
+            float farHalfHeight = m_farDist * m_focalLengthInv;
+            float farHalfWidth = farHalfHeight * m_aspectRatio;
 
+            // 顶点位置是 0 - near x left x top
+            // 顶点位置是 1 - near x right x top
+            // 顶点位置是 2 - near x right x bottom
+            // 顶点位置是 3 - near x left x bottom
+            // 顶点位置是 4 - far x left x top
+            // 顶点位置是 5 - far x right x top
+            // 顶点位置是 6 - far x right x bottom
+            // 顶点位置是 7 - far x left x bottom
+            // 0 - 1
+            // |   |
+            // 3 - 2
             m_frustumCorners[0] = m_frustumCorners[9] = left;
 			m_frustumCorners[3] = m_frustumCorners[6] = right;
 			m_frustumCorners[1] = m_frustumCorners[4] = top;
 			m_frustumCorners[7] = m_frustumCorners[10] = bottom;
 			
-			m_frustumCorners[12] = m_frustumCorners[21] = -xMaxFar;
-			m_frustumCorners[15] = m_frustumCorners[18] = xMaxFar;
-			m_frustumCorners[13] = m_frustumCorners[16] = -yMaxFar;
-			m_frustumCorners[19] = m_frustumCorners[22] = yMaxFar;
+			m_frustumCorners[12] = m_frustumCorners[21] = -farHalfWidth;
+			m_frustumCorners[15] = m_frustumCorners[18] = farHalfWidth;
+			m_frustumCorners[13] = m_frustumCorners[16] = farHalfHeight;
+			m_frustumCorners[19] = m_frustumCorners[22] = -farHalfHeight;
 			
 			m_frustumCorners[2] = m_frustumCorners[5] = m_frustumCorners[8] = m_frustumCorners[11] = m_nearDist;
 			m_frustumCorners[14] = m_frustumCorners[17] = m_frustumCorners[20] = m_frustumCorners[23] = m_farDist;
