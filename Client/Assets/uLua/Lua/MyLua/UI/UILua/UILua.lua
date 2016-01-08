7bc8f9181b3312bab923d318b117e26bcaa62c8b
aaa <kuangben2001@163.com>
2016-01-08 13:46:27 +0800

    aaa

--- Client/Assets/uLua/Lua/MyLua/UI/UILua/UILua.lua
+++ Client/Assets/uLua/Lua/MyLua/UI/UILua/UILua.lua
@@ -17,6 +17,7 @@ function M:onInit()
 end
 
 function M:onReady()
+    -- TODO: 之前使用的是 self.super ，结果如果子类中没有实现这个函数，然后这个函数又被调用了，结果直接调用父类的，而父类中有会再次调用 self 的 super 的函数，结果就死循环了，不断的调用自己
     -- self.super.onReady(self)
     M.super.onReady(self)
     print("M:onReady()")
