357b9ffb6affef8a03d72dbc2f176a519de738a2
codekuangben <kuangben2001@163.com>
2016-02-06 21:32:15 +0800

    aaa

--- Client/Assets/uLua/Lua/MyLua/Test/TestCommon/main.lua
+++ Client/Assets/uLua/Lua/MyLua/Test/TestCommon/main.lua
@@ -1,10 +1,10 @@
 package.path = string.format("%s;%s/?.lua", package.path, "F:/File/opensource/unity-game-git/unitygame/unitygame/Client/Assets/uLua/Lua")
 --package.path = string.format("%s;%s/?.lua", package.path, "E:/Self/Self/unity/unitygame/Client/Assets/uLua/Lua")
 
-require "MyLua.Libs.Core.Prequisites"
-require "MyLua.Test.TestCommon.TestMisc"
-require "MyLua.Test.TestCommon.TestLinkList"
-require "MyLua.Test.TestCommon.TestCoroutine"
+--require "MyLua.Libs.Core.Prequisites"
+--require "MyLua.Test.TestCommon.TestMisc"
+--require "MyLua.Test.TestCommon.TestLinkList"
+--require "MyLua.Test.TestCommon.TestCoroutine"
 
 local m_testMisc = GlobalNS.new(GlobalNS.TestMisc);
 local m_testLinkList = GlobalNS.new(GlobalNS.TestLinkList);
