3141aa2e1b002401019ab200f3dc416bcd5bdeeb
aaa <kuangben2001@163.com>
2016-02-19 14:22:55 +0800

    aaa

---
+++ Client/Assets/Scripts/Libs/UI/UICore/CanvasMatch.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SDK.Lib
+{
+    /**
+     * @屏幕自适应
+     */
+    public class CanvasMatch
+    {
+        protected int m_refWidth;
+        protected int m_refHeight;
+        protected CanvasScaler m_canvasScaler;
+        protected int m_screenWidth = 0;
+        protected int m_screenHeight = 0;
+        protected GameObject m_go;
+
+        public CanvasMatch()
+        {
+
+        }
+
+        public void init()
+        {
+            m_canvasScaler = m_go.GetComponent<CanvasScaler>();
+            updateScaleMatch();
+        }
+
+        public void update()
+        {
+            if(Application.isEditor)
+            {
+                updateScaleMatch();
+            }
+        }
+
+        public void updateScaleMatch()
+        {
+            m_screenWidth = Screen.width;
+            m_screenHeight = Screen.height;
+
+            float refRate = ((float)m_refWidth / m_refHeight);
+            float screenRate = ((float)m_screenWidth / m_screenHeight);
+            m_canvasScaler.matchWidthOrHeight = screenRate >= refRate ? 1 : 0;
+        }
+    }
+}
