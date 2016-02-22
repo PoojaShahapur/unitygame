4ecceed3d15f47594633d7ce59a06826a735ce74
aaa <kuangben2001@163.com>
2016-02-22 17:37:39 +0800

    修改坐标

--- Client/Assets/Scripts/Libs/Math/SphericalCoordinate.cs
+++ Client/Assets/Scripts/Libs/Math/SphericalCoordinate.cs
@@ -12,12 +12,17 @@ namespace SDK.Lib
         protected float m_theta;        // θ∈[0, π]，与 Y 夹角
         protected float m_fai;          // φ∈[0,2π]，与 X 夹角
 
+        protected bool m_bHalfMode;   // 半球模式
+        protected bool m_bOrderMode; // 是否是相反的模式，就是操作摄像机的时候，是增加还是减少
+
         protected float m_x;
         protected float m_y;
         protected float m_z;
 
         public SphericalCoordinate()
         {
+            m_bOrderMode = true;
+            m_bHalfMode = true;
             m_radius = 5;
             m_theta = Mathf.PI / 4;
             m_radius = Mathf.PI;
@@ -44,29 +49,105 @@ namespace SDK.Lib
         // 增加 theta
         override public void incTheta(float deltaDegree)
         {
+            if(m_bOrderMode)
+            {
+                incThetaInternal(deltaDegree);
+            }
+            else
+            {
+                decThetaInternal(deltaDegree);
+            }
+        }
+
+        protected void incThetaInternal(float deltaDegree)
+        {
             m_theta += deltaDegree * Mathf.Deg2Rad;
             //m_theta += deltaDegree;
             //m_theta %= Mathf.PI;
+            // 如果超过范围
             if (m_theta > Mathf.PI)
             {
-                //m_theta = Mathf.PI;
-                m_theta = Mathf.PI - (m_theta - Mathf.PI);
-                m_fai += Mathf.PI;
-                m_fai %= (2 * Mathf.PI);
+                if (m_bHalfMode)
+                {
+                    m_theta = Mathf.PI;
+                }
+                else
+                {
+                    /*
+                    // 如果增加的比较大
+                    m_bOrderMode = !m_bOrderMode;
+                    //m_theta = Mathf.PI;
+                    m_theta = Mathf.PI - (m_theta - Mathf.PI);
+                    m_fai += Mathf.PI;
+                    m_fai %= (2 * Mathf.PI);
+                    */
+
+                    // 如果范围取模后，最终在 (Mathf.PI, 2 * Mathf.PI)
+                    float mod = m_theta % (2 * Mathf.PI);
+                    if (mod > Mathf.PI)
+                    {
+                        m_bOrderMode = !m_bOrderMode;
+                        m_theta = Mathf.PI - (m_theta - Mathf.PI);
+                        m_fai += Mathf.PI;
+                        m_fai %= (2 * Mathf.PI);
+                    }
+                    else
+                    {
+                        // 如果又进入 [0, Mathf.PI]
+                        m_theta = mod;
+                    }
+                }
             }
         }
 
         // 减少 theta
         override public void decTheta(float deltaDegree)
         {
+            if (m_bOrderMode)
+            {
+                decThetaInternal(deltaDegree);
+            }
+            else
+            {
+                incThetaInternal(deltaDegree);
+            }
+        }
+
+        protected void decThetaInternal(float deltaDegree)
+        {
             m_theta -= deltaDegree * Mathf.Deg2Rad;
             //m_theta -= deltaDegree;
             if (m_theta < 0)
             {
-                //m_theta = 0;
-                m_theta = -m_theta;
-                m_fai += Mathf.PI;
-                m_fai %= (2 * Mathf.PI);
+                if (m_bHalfMode)
+                {
+                    m_theta = 0;
+                }
+                else
+                {
+                    /*
+                    m_bOrderMode = !m_bOrderMode;
+                    //m_theta = 0;
+                    m_theta = -m_theta;
+                    m_fai += Mathf.PI;
+                    m_fai %= (2 * Mathf.PI);
+                    */
+
+                    float modAbs = Mathf.Abs(m_theta % (2 * Mathf.PI));
+                    // 如果减到另外一面
+                    if (modAbs < Mathf.PI)
+                    {
+                        m_bOrderMode = !m_bOrderMode;
+                        m_theta = -m_theta;
+                        m_fai += Mathf.PI;
+                        m_fai %= (2 * Mathf.PI);
+                    }
+                    else
+                    {
+                        // 还是在原来一面
+                        m_theta = Mathf.PI - (modAbs - Mathf.PI);
+                    }
+                }
             }
         }
 
