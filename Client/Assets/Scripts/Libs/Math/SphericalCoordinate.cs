45b797f90b4ffd204fa3ecc632fdd5dd6c1fe794
aaa <kuangben2001@163.com>
2016-02-22 16:54:56 +0800

    修改摄像机控制器

--- Client/Assets/Scripts/Libs/Math/SphericalCoordinate.cs
+++ Client/Assets/Scripts/Libs/Math/SphericalCoordinate.cs
@@ -9,7 +9,7 @@ namespace SDK.Lib
     {
         // 球形坐标系，设置的旋转角度，最后都变成了空间中的坐标，因此不需要进行旋转
         protected float m_radius;       // r∈[0,+∞)，半径
-        protected float m_theta;        // θ∈[0, π]，与 Z 夹角
+        protected float m_theta;        // θ∈[0, π]，与 Y 夹角
         protected float m_fai;          // φ∈[0,2π]，与 X 夹角
 
         protected float m_x;
@@ -45,10 +45,14 @@ namespace SDK.Lib
         override public void incTheta(float deltaDegree)
         {
             m_theta += deltaDegree * Mathf.Deg2Rad;
+            //m_theta += deltaDegree;
             //m_theta %= Mathf.PI;
-            if(m_theta > Mathf.PI)
+            if (m_theta > Mathf.PI)
             {
-                m_theta = Mathf.PI;
+                //m_theta = Mathf.PI;
+                m_theta = Mathf.PI - (m_theta - Mathf.PI);
+                m_fai += Mathf.PI;
+                m_fai %= (2 * Mathf.PI);
             }
         }
 
@@ -56,9 +60,13 @@ namespace SDK.Lib
         override public void decTheta(float deltaDegree)
         {
             m_theta -= deltaDegree * Mathf.Deg2Rad;
-            if(m_theta < 0)
+            //m_theta -= deltaDegree;
+            if (m_theta < 0)
             {
-                m_theta = 0;
+                //m_theta = 0;
+                m_theta = -m_theta;
+                m_fai += Mathf.PI;
+                m_fai %= (2 * Mathf.PI);
             }
         }
 
@@ -79,8 +87,8 @@ namespace SDK.Lib
         override public void convSpherical2Cartesian()
         {
             m_x = m_radius * Mathf.Sin(m_theta) * Mathf.Cos(m_fai);
-            m_y = m_radius * Mathf.Sin(m_theta) * Mathf.Sin(m_fai);
-            m_z = m_radius * Mathf.Cos(m_theta);
+            m_y = m_radius * Mathf.Cos(m_theta);
+            m_z = m_radius * Mathf.Sin(m_theta) * Mathf.Sin(m_fai);
         }
 
         override public void syncTrans(Transform trans)
--- Client/Assets/Scripts/Libs/Scene/Camera/ThirdCameraController.cs
+++ Client/Assets/Scripts/Libs/Scene/Camera/ThirdCameraController.cs
@@ -17,7 +17,8 @@ namespace SDK.Lib
 
             //this.setParam(5, 45, 180);
             //this.setParam(10, 45, 0);
-            this.setParam(10, 45, 45);
+            //this.setParam(10, 45, 45);
+            this.setParam(10, 90, 0);
         }
 
         // 增加 theta
