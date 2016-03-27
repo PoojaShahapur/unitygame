be738de25f49a1cfc9f2373dab41a81e0693485c
codekuangben <kuangben2001@163.com>
2016-03-27 21:12:44 +0800

    aaa

---
+++ Client/Assets/Editor/ExportTerrain/ExportTerrainSplat.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace EditorTool
+{
+    public class ExportTer
+    {
+        [MenuItem("My/Assets/Export Texture")]
+        static public void Apply()
+        {
+            Texture2D texture = Selection.activeObject as Texture2D;
+            if (texture == null)
+            {
+                EditorUtility.DisplayDialog("Select Texture", "You Must Select a Texture first!", "Ok");
+                return;
+            }
+
+            byte[] bytes = texture.EncodeToPNG();
+            File.WriteAllBytes(Application.dataPath + "/exported_texture.png", bytes);
+        }
+    }
+}
