d87bdd54140a3b73b22eb4d21374cb9143f04788
aaa <kuangben2001@163.com>
2016-04-08 10:44:13 +0800

    aaa

--- Client/Assets/Scripts/Libs/Scene/Terrain/TerrainEntity/TerrainMesh/TerrainGlobalOption.cs
+++ Client/Assets/Scripts/Libs/Scene/Terrain/TerrainEntity/TerrainMesh/TerrainGlobalOption.cs
@@ -23,7 +23,7 @@
 
         public TerrainGlobalOption()
         {
-            mTerrainWorldSizeInAll = 200;
+            mTerrainWorldSizeInAll = 3000;
             mTerrainSize = 65;
             mTerrainWorldSize = 200;
             mTerrainPageCount = (int)(mTerrainWorldSizeInAll / mTerrainWorldSize);
@@ -33,7 +33,7 @@
             mInputScale = 200;
             mInputBias = 0;
 
-            mIsReadFile = false;
+            mIsReadFile = true;
             mNeedCull = true;
             mNeedSaveScene = false;
 
