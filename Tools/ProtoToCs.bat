85818f7aa91f44a31a6b127a716e668d42b69ac9
codekuangben <kuangben2001@163.com>
2016-05-04 00:53:42 +0800

    aaa

--- Tools/ProtoToCs.bat
+++ Tools/ProtoToCs.bat
@@ -1,7 +1,7 @@
 cd %~dp0
 %~d0
 
-ProtoGen\protogen.exe -i:..\Msg\Proto\User.proto -o:..\Msg\Cs\User.cs
-ProtoGen\protoc.exe --proto_path=. -o ..\Msg\Pb\User.pb ..\Msg\Proto\User.proto
+ProtoGenExe\protogen.exe --proto_path=. -i:..\Msg\Proto\User.proto -o:..\Msg\Cs\User.cs
+ProtoGenExe\protoc.exe --proto_path=. -o ..\Msg\Pb\User.pb ..\Msg\Proto\User.proto
 
 pause
