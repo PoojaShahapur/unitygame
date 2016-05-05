0e2b64410cda7f6d6447242f45828f143fc9f539
codekuangben <kuangben2001@163.com>
2016-05-06 00:08:45 +0800

    aaa

---
+++ Client/Assets/uLua/Lua/MyLua/Libs/AuxComponent/AuxComponent.lua
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+
+namespace SDK.Lib
+{
+    /**
+     * @brief 辅助基类
+     */
+    public class AuxComponent : IDispatchObject
+    {
+        protected GameObject m_selfGo;      // 自己节点
+        protected GameObject m_pntGo;       // 指向父节点
+        protected GameObject m_placeHolderGo;      // 自己节点，资源挂在 m_placeHolderGo 上， m_placeHolderGo 挂在 m_pntGo 上
+        protected bool m_bNeedPlaceHolderGo;    // 是否需要占位 GameObject
+        protected LuaCSBridge m_luaCSBridge;
+
+        public AuxComponent(LuaCSBridge luaCSBridge_ = null)
+        {
+            m_luaCSBridge = luaCSBridge_;
+            m_bNeedPlaceHolderGo = false;
+        }
+
+        public void setSelfName(string name_)
+        {
+            this.selfGo.name = name_;
+        }
+
+        public GameObject selfGo
+        {
+            get
+            {
+                return m_selfGo;
+            }
+            set
+            {
+                bool bPntChange = bChange(m_selfGo, value);
+                m_selfGo = value;
+                if (bPntChange)
+                {
+                    onSelfChanged();
+                }
+            }
+        }
+
+        public GameObject pntGo
+        {
+            get
+            {
+                return m_pntGo;
+            }
+            set
+            {
+                bool bPntChange = bChange(m_pntGo, value);
+                m_pntGo = value;
+                if (bPntChange)
+                {
+                    onPntChanged();
+                }
+            }
+        }
+
+        public virtual void setPntGo(GameObject go)
+        {
+            m_pntGo = go;
+        }
+
+        public bool bNeedPlaceHolderGo
+        {
+            get
+            {
+                return m_bNeedPlaceHolderGo;
+            }
+            set
+            {
+                m_bNeedPlaceHolderGo = value;
+                if(m_bNeedPlaceHolderGo)
+                {
+                    if (m_placeHolderGo == null)
+                    {
+                        m_placeHolderGo = UtilApi.createGameObject("PlaceHolderGO");
+                    }
+                }
+            }
+        }
+
+        public GameObject placeHolderGo
+        {
+            get
+            {
+                return m_placeHolderGo;
+            }
+            set
+            {
+                m_placeHolderGo = value;
+            }
+        }
+
+        public bool isSelfValid()
+        {
+            return this.m_selfGo != null;
+        }
+
+        virtual public void dispose()
+        {
+            if (m_bNeedPlaceHolderGo && m_placeHolderGo != null)
+            {
+                UtilApi.Destroy(m_placeHolderGo);
+            }
+        }
+
+        protected bool bChange(GameObject srcGO, GameObject destGO)
+        {
+            if (srcGO == null || !srcGO.Equals(destGO))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        // 父节点发生改变
+        virtual protected void onPntChanged()
+        {
+            linkSelf2Parent();
+        }
+
+        // 自己发生改变
+        virtual protected void onSelfChanged()
+        {
+            linkSelf2Parent();
+        }
+
+        public void linkPlaceHolder2Parent()
+        {
+            if (m_placeHolderGo == null)
+            {
+                m_placeHolderGo = UtilApi.createGameObject("PlaceHolderGO");
+            }
+            UtilApi.SetParent(m_placeHolderGo, m_pntGo, false);
+        }
+
+        public void linkSelf2Parent()
+        {
+            if (m_selfGo != null && m_pntGo != null)   // 现在可能还没有创建
+            {
+                UtilApi.SetParent(m_selfGo, m_pntGo, false);
+            }
+        }
+
+        virtual public void show()
+        {
+            if (m_selfGo != null)
+            {
+                UtilApi.SetActive(m_selfGo, true);
+            }
+        }
+
+        public void hide()
+        {
+            if (m_selfGo != null && this.IsVisible())
+            {
+                UtilApi.SetActive(m_selfGo, false);
+            }
+        }
+
+        public bool IsVisible()
+        {
+            return UtilApi.IsActive(m_selfGo);
+        }
+    }
+}
---
+++ Client/Assets/uLua/Lua/MyLua/Libs/AuxComponent/AuxModelComponent/AuxPrefabComponent.lua
@@ -0,0 +1,146 @@
+﻿using System;
+
+namespace SDK.Lib
+{
+    /**
+     * @brief 移动
+     */
+    public class AuxPrefabComponent : AuxComponent
+    {
+        protected PrefabRes mPrefabRes;                     // 预制资源
+        protected ResEventDispatch mEvtHandle;              // 事件分发器
+        protected ResInsEventDispatch mResInsEventDispatch; // 实例化的时候使用的分发器
+        protected bool mIsSuccess;      // 是否成功
+        protected string mPath;         // 加载的资源目录
+        protected bool mIsInsNeedCoroutine; // 实例化是否需要协程
+
+        public AuxPrefabComponent(bool isInsNeedCoroutine = true)
+            : base(null)
+        {
+            mIsInsNeedCoroutine = isInsNeedCoroutine;
+            mIsSuccess = false;
+            mPath = "";
+        }
+
+        override public void dispose()
+        {
+            unload();
+            if(this.selfGo != null)
+            {
+                UtilApi.Destroy(this.selfGo);
+            }
+            base.dispose();
+        }
+
+        public bool hasSuccessLoaded()
+        {
+            return mIsSuccess;
+        }
+
+        public bool hasFailed()
+        {
+            return !mIsSuccess;
+        }
+
+        public string GetPath()
+        {
+            if(mPrefabRes != null)
+            {
+                return mPrefabRes.GetPath();
+            }
+
+            return mPath;
+        }
+
+        // 异步加载对象
+        public void asyncLoad(string path, Action<IDispatchObject> dispObj)
+        {
+            if(mPath != path)
+            {
+                unload();
+                mPath = path;
+                mEvtHandle = new ResEventDispatch();
+                mEvtHandle.addEventHandle(dispObj);
+                mPrefabRes = Ctx.m_instance.m_prefabMgr.getAndAsyncLoadRes(path, onPrefabLoaded);
+            }
+        }
+
+        public void onPrefabLoaded(IDispatchObject dispObj)
+        {
+            // 一定要从这里再次取值，因为如果这个资源已经加载，可能在返回之前就先调用这个函数，因此这个时候 mPrefabRes 还是空值
+            mPrefabRes = dispObj as PrefabRes;
+            if(mPrefabRes.hasSuccessLoaded())
+            {
+                mIsSuccess = true;
+                if (mIsInsNeedCoroutine)
+                {
+                    mResInsEventDispatch = new ResInsEventDispatch();
+                    mResInsEventDispatch.addEventHandle(onPrefabIns);
+                    mPrefabRes.InstantiateObject(mPrefabRes.GetPath(), mResInsEventDispatch);
+                }
+                else
+                {
+                    this.selfGo = mPrefabRes.InstantiateObject(mPrefabRes.GetPath());
+                    onAllFinish();
+                }
+            }
+            else if (mPrefabRes.hasFailed())
+            {
+                mIsSuccess = false;
+                Ctx.m_instance.m_prefabMgr.unload(mPrefabRes.GetPath(), onPrefabLoaded);
+                mPrefabRes = null;
+
+                if (mEvtHandle != null)
+                {
+                    mEvtHandle.dispatchEvent(this);
+                }
+            }
+        }
+
+        public void onPrefabIns(IDispatchObject dispObj)
+        {
+            mResInsEventDispatch = dispObj as ResInsEventDispatch;
+            this.selfGo = mResInsEventDispatch.getInsGO();
+            onAllFinish();
+        }
+
+        // 所有的资源都加载完成
+        public void onAllFinish()
+        {
+            if (this.selfGo != null)
+            {
+                mIsSuccess = true;
+            }
+            else
+            {
+                mIsSuccess = false;
+            }
+
+            if (mEvtHandle != null)
+            {
+                mEvtHandle.dispatchEvent(this);
+            }
+        }
+
+        public void unload()
+        {
+            if(mPrefabRes != null)
+            {
+                Ctx.m_instance.m_prefabMgr.unload(mPrefabRes.GetPath(), onPrefabLoaded);
+                mPrefabRes = null;
+            }
+
+            if(mResInsEventDispatch != null)
+            {
+                mResInsEventDispatch.setIsValid(false);
+                mResInsEventDispatch = null;
+            }
+
+            if (mEvtHandle != null)
+            {
+                mEvtHandle.clearEventHandle();
+                mEvtHandle = null;
+            }
+        }
+    }
+}
