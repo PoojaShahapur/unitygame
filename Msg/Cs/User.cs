917fc1f041c6ee3f2edccd6a9594a43cfa670cd1
aaa <kuangben2001@163.com>
2016-05-18 11:20:25 +0800

    aaa

--- Msg/Cs/User.cs
+++
@@ -1,8 +0,0 @@
-message User
-{
-	required int32     uid  = 1;
-	optional int64     guid = 2;
-	optional byte      nick = 3;
-	optional string    account = 4;
-	optional UserType  type = 5[default=1];
-}
