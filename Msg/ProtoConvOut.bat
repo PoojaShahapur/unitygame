8a2f85a846ae4b4ef46c4ab5e277655276df8bb1
aaa <kuangben2001@163.com>
2016-05-30 19:55:14 +0800

    添加测试 proto

--- Msg/ProtoConvOut.bat
+++ Msg/ProtoConvOut.bat
@@ -3,5 +3,7 @@ cd %~dp0
 
 ..\Tools\ProtoGenExe\protogen.exe -i:Proto\Test.proto -o:Cs\Test.cs
 ..\Tools\ProtoGenExe\protoc.exe Proto\Test.proto -o Pb\Test.pb
+..\Tools\ProtoGenExe\protoc.exe Proto\login.proto -o Pb\login.pb
+..\Tools\ProtoGenExe\protoc.exe Proto\rpc.proto -o Pb\rpc.pb
 
 pause
