c5809c351609ba8ca720678e4b96bd3b52fbf0cb
aaa <kuangben2001@163.com>
2016-06-08 16:32:04 +0800

    修改工具

--- Tools/BuildAndroidApk.bat
+++ Tools/BuildAndroidApk.bat
@@ -1,7 +1,7 @@
 rem UNITY程序的路径
-set UNITY_PATH=/Applications/Unity/Unity.app/Contents/MacOS/Unity
+set UNITY_PATH=D:\ProgramFiles\Unity\Editor\Unity.exe
  
 rem 游戏程序路径
-set PROJECT_PATH=/Users/MOMO/commond
+set PROJECT_PATH=E:\Self\Self\unity\unitygame\Client_Start
 
-%UNITY_PATH% -projectPath %PROJECT_PATH% -executeMethod ProjectBuild.BuildForAndroid project-%1 -quit
+%UNITY_PATH% -quit -batchmode -nographics -logFile E:\Self\Self\unity\unitygame\Client_Start\BuildOut\EditorLog.txt -projectPath %PROJECT_PATH% -executeMethod EditorTool.CmdSys.cmdMain nihao=ceshi
