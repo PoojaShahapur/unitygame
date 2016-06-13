fa55b168641a532df5eea75117ed4d7e908c3ba5
aaa <kuangben2001@163.com>
2016-06-13 12:24:41 +0800

    提交工具

---
+++ Client_Start/Assets/Scripts/Libs/Resource/Download/WWWDownloadItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace SDK.Lib
+{
+    /**
+    * @brief 使用 Unity 的 WWW 从网络下载数据
+    */
+    public class WWWDownloadItem : DownloadItem
+    {
+        public WWWDownloadItem()
+        {
+
+        }
+
+        override public void load()
+        {
+            base.load();
+
+            Ctx.m_instance.m_coroutineMgr.StartCoroutine(downloadAsset());
+        }
+
+        // m_path 是这个格式 http://127.0.0.1/UnityServer/Version.txt?ver=100
+        override protected IEnumerator downloadAsset()
+        {
+            deleteFromCache(m_loadPath);
+            m_w3File = WWW.LoadFromCacheOrDownload(m_loadPath, Int32.Parse(m_version));
+            yield return m_w3File;
+
+            onWWWEnd();
+        }
+
+        // 加载完成回调处理
+        override protected void onWWWEnd()
+        {
+            if (isLoadedSuccess(m_w3File))
+            {
+                m_bytes = m_w3File.bytes;
+
+                m_nonRefCountResLoadResultNotify.resLoadState.setSuccessLoaded();
+            }
+            else
+            {
+                m_nonRefCountResLoadResultNotify.resLoadState.setFailed();
+            }
+            m_nonRefCountResLoadResultNotify.loadResEventDispatch.dispatchEvent(this);
+        }
+    }
+}
