0c33f809fdb2d11bace796ca319f5e614366edb4
aaa <kuangben2001@163.com>
2016-11-22 18:08:20 +0800

    添加 KBE 消息处理

---
+++ Client_Start/Assets/Scripts/Module/Login/EventCB/KBELoginNetHandleCB.cs
@@ -0,0 +1,23 @@
+﻿using SDK.Lib;
+
+namespace Game.Login
+{
+    /**
+     * @brief KBEngine 登陆网络处理
+     */
+    public class KBELoginNetHandleCB : NetModuleDispHandle
+    {
+        public KBELoginNetHandleCB()
+        {
+            NetCmdDispHandle cmdHandle = null;
+            cmdHandle = new LoginTimerUserCmdHandle();
+            this.addCmdHandle(stNullUserCmd.TIME_USERCMD, cmdHandle, cmdHandle.call);
+        }
+
+        // 如果要调试可以重载，方便调试
+        //public override void handleMsg(ByteBuffer msg)
+        //{
+
+        //}
+    }
+}
