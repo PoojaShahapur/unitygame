0df38ea685325020a02a2c3de51e8faf8ad82abc
aaa <kuangben2001@163.com>
2016-12-12 11:17:16 +0800

    提交

--- Client_Start/Assets/Scripts/Libs/LuaCS/LuaCSBridge/LuaScriptMgr.cs
+++ Client_Start/Assets/Scripts/Libs/LuaCS/LuaCSBridge/LuaScriptMgr.cs
@@ -160,18 +160,19 @@ namespace SDK.Lib
                 loader.AddBundle("lua/lua_system.unity3d");
                 loader.AddBundle("lua/lua_system_reflection.unity3d");
                 loader.AddBundle("lua/lua_unityengine.unity3d");
-                loader.AddBundle("lua/lua_common.unity3d");
-                loader.AddBundle("lua/lua_logic.unity3d");
-                loader.AddBundle("lua/lua_view.unity3d");
-                loader.AddBundle("lua/lua_controller.unity3d");
-                loader.AddBundle("lua/lua_misc.unity3d");
 
-                loader.AddBundle("lua/lua_protobuf.unity3d");
-                loader.AddBundle("lua/lua_3rd_cjson.unity3d");
-                loader.AddBundle("lua/lua_3rd_luabitop.unity3d");
+                //loader.AddBundle("lua/lua_common.unity3d");
+                //loader.AddBundle("lua/lua_logic.unity3d");
+                //loader.AddBundle("lua/lua_view.unity3d");
+                //loader.AddBundle("lua/lua_controller.unity3d");
+                //loader.AddBundle("lua/lua_misc.unity3d");
+
+                //loader.AddBundle("lua/lua_protobuf.unity3d");
+                //loader.AddBundle("lua/lua_3rd_cjson.unity3d");
+                //loader.AddBundle("lua/lua_3rd_luabitop.unity3d");
                 loader.AddBundle("lua/lua_3rd_pbc.unity3d");
-                loader.AddBundle("lua/lua_3rd_pblua.unity3d");
-                loader.AddBundle("lua/lua_3rd_sproto.unity3d");
+                //loader.AddBundle("lua/lua_3rd_pblua.unity3d");
+                //loader.AddBundle("lua/lua_3rd_sproto.unity3d");
             }
         }
 
