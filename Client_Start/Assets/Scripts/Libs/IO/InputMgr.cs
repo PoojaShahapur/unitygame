5f46fb489f9822a7d6c086a35deacb9e0fb0357f
codekuangben <kuangben2001@163.com>
2016-12-13 22:33:11 +0800

    aaa

--- Client_Start/Assets/Scripts/Libs/IO/InputMgr.cs
+++ Client_Start/Assets/Scripts/Libs/IO/InputMgr.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace SDK.Lib
 {
@@ -8,10 +7,10 @@ namespace SDK.Lib
      */
     public class InputMgr : ITickedObject, IDelayHandleItem
     {
-        public MControlScheme mCurrentScheme = MControlScheme.Mouse;
-        public bool mAllowMultiTouch = false;
-        public int mCurrentTouchID = -1;
-        public MTouch mCurrentTouch = null;
+        public MControlScheme mCurrentScheme;
+        public bool mAllowMultiTouch;
+        public int mCurrentTouchID;
+        public MTouch mCurrentTouch;
 
         // 有监听事件的键盘 InputKey
         protected MList<InputKey> mEventInputKeyList;
@@ -21,12 +20,20 @@ namespace SDK.Lib
         protected bool mHasAccelerationHandle;
 
         //private Action mOnAxisDown = null;
+        protected bool mIsEnableIO;     // 是否开启输入
 
         public InputMgr()
         {
+            this.mCurrentScheme = MControlScheme.Mouse;
+            this.mAllowMultiTouch = false;
+            this.mCurrentTouchID = -1;
+            this.mCurrentTouch = null;
+
             this.mEventInputKeyList = new MList<InputKey>();
             this.mEventMouseList = new MList<MMouse>();
             this.mHasAccelerationHandle = false;
+
+            this.mIsEnableIO = true;
         }
 
         public void init()
@@ -57,27 +64,30 @@ namespace SDK.Lib
          */
         public void onTick(float delta)
         {
-            //handleAxis();
-
-            // This function tracks which keys were just pressed (or released) within the last tick.
-            // It should be called at the beginning of the tick to give the most accurate responses possible.
-            int idx = 0;
-            
-            for (idx = 0; idx < this.mEventInputKeyList.Count(); idx++)
+            if (this.mIsEnableIO)
             {
-                this.mEventInputKeyList[idx].onTick(delta);
-            }
+                //handleAxis();
 
-            for (idx = 0; idx < this.mEventMouseList.Count(); idx++)
-            {
-                this.mEventMouseList[idx].onTick(delta);
-            }
+                // This function tracks which keys were just pressed (or released) within the last tick.
+                // It should be called at the beginning of the tick to give the most accurate responses possible.
+                int idx = 0;
 
-            this.ProcessTouches(delta);
+                for (idx = 0; idx < this.mEventInputKeyList.Count(); idx++)
+                {
+                    this.mEventInputKeyList[idx].onTick(delta);
+                }
 
-            if(this.mHasAccelerationHandle)
-            {
-                MAcceleration.mAccelerationOne.onTick(delta);
+                for (idx = 0; idx < this.mEventMouseList.Count(); idx++)
+                {
+                    this.mEventMouseList[idx].onTick(delta);
+                }
+
+                this.ProcessTouches(delta);
+
+                if (this.mHasAccelerationHandle)
+                {
+                    MAcceleration.mAccelerationOne.onTick(delta);
+                }
             }
         }
 
---
+++ Client_Start/Assets/Scripts/Libs/IO/MInputAxis.cs
@@ -0,0 +1,10 @@
+﻿namespace SDK.Lib
+{
+    /**
+     * @brief Edit -- Project Settings -- Input 中设置的 Axis
+     */
+    public class MInputAxis : IDispatchObject
+    {
+
+    }
+}
