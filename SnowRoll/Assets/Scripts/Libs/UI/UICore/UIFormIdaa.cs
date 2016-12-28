05177b48c36e8f503c51937b4ce723c992fcbb7a
codekuangben <kuangben2001@163.com>
2016-12-28 08:09:12 +0800

    aaa

---
+++ SnowRoll/Assets/Scripts/Libs/UI/UICore/UIFormIdaa.cs
@@ -0,0 +1,23 @@
+namespace SDK.Lib
+{
+	/**
+	 * 界面ID定义
+	 */
+	public enum UIFormID
+	{
+        // 第二层
+        eUITest,            // 测试界面
+        eUITerrainEdit,     // 地形编辑器窗口
+        eUILogin,           // 登陆窗口
+        eUISelectRole,      // 选择角色窗口
+        eUIPack,            // 背包界面
+
+        // Lua 界面 Id 定义开始
+        eUIStartGame_Lua = 10001,       // Lua中的界面
+    }
+
+    public enum WinIdCnt
+    {
+        eIDCnt = 500,           // 每一个窗口中可以占用的 ID 数量
+    }
+}
