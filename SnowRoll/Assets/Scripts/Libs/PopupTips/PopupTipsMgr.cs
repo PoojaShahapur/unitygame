e5034ecc3e68ac06a8a01463d61d4bbfb79eb3f3
codekuangben <kuangben2001@163.com>
2017-02-11 00:18:38 +0800

    修改

--- SnowRoll/Assets/Scripts/Libs/PopupTips/PopupTipsMgr.cs
+++ SnowRoll/Assets/Scripts/Libs/PopupTips/PopupTipsMgr.cs
@@ -2,6 +2,21 @@
 {
     public class PopupTipsMgr
     {
-        protected MList<PopupTipsItemBase> mList; 
+        protected MList<PopupTipsItemBase> mList;
+
+        public PopupTipsMgr()
+        {
+            this.mList = new MList<PopupTipsItemBase>();
+        }
+
+        public void init()
+        {
+
+        }
+
+        public void dispose()
+        {
+
+        }
     }
 }
