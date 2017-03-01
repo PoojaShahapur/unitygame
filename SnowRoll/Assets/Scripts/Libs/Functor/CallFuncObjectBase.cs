be38319d14d9b3f88bfa90eb5630d1dbcc702897
吕广海 <kuangben2001@163.com>
2017-03-01 13:46:23 +0800

    添加 functor

---
+++ SnowRoll/Assets/Scripts/Libs/Functor/CallFuncObjectBase.cs
@@ -0,0 +1,51 @@
+﻿namespace SDK.Lib
+{
+    public class CallFuncObjectBase
+    {
+        protected ICalleeObject mThis;
+        protected MAction<IDispatchObject> mHandle;
+        protected IDispatchObject mParam;
+
+        public CallFuncObjectBase()
+        {
+            this.mHandle = null;
+            this.mThis = null;
+            this.mParam = null;
+        }
+
+        public void setPThisAndHandle(ICalleeObject pThis, MAction<IDispatchObject> handle, IDispatchObject param)
+        {
+            this.mHandle = handle;
+            this.mThis = pThis;
+            this.mParam = param;
+        }
+
+        public void clear()
+        {
+            this.mThis = null;
+            this.mHandle = null;
+            this.mParam = null;
+        }
+
+        public bool isValid()
+        {
+            if (null != this.mThis && null != this.mHandle)
+            {
+                return true;
+            }
+            else if (null != this.mHandle)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void call()
+        {
+
+        }
+    }
+}
